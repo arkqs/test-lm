8035b90a30981c5cf1b838dcbba4a328754934a8
Quentin <quentinsagardoy7@gmail.com>
2024-08-08 14:36:15 +0200

    update

--- api/src/main/java/com/example/api/model/Produit.java
+++ api/src/main/java/com/example/api/model/Produit.java
@@ -17,23 +17,11 @@ public class Produit {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNom() {
         return nom;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
     public float getPrix() {
         return prix;
-    }
-
-    public void setPrix(float prix) {
-        this.prix = prix;
     }
 }
