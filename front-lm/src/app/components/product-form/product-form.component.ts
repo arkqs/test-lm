2fa771335efa705f024f2640f56d2b8308838a3e
Quentin <quentinsagardoy7@gmail.com>
2024-08-08 13:57:48 +0200

    add prettier

--- front-lm/src/app/components/product-form/product-form.component.ts
+++ front-lm/src/app/components/product-form/product-form.component.ts
@@ -1,34 +1,41 @@
-import { Component } from '@angular/core'
-import { ProductService } from '../../core/service/product.service'
-import { FormsModule } from '@angular/forms'
-import { CommonModule } from '@angular/common'
+import { Component } from "@angular/core";
+import { ProductService } from "../../core/service/product.service";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
-    selector: 'product-form',  
-    templateUrl: './product-form.component.html',
-    styleUrls: ['./product-form.component.css'],
-    standalone: true,
-    imports: [FormsModule, CommonModule]
+  selector: "product-form",
+  templateUrl: "./product-form.component.html",
+  styleUrls: ["./product-form.component.css"],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
 })
 export class ProductFormComponent {
-    nom: string = ''
-    prix: number = 0
+  nom: string = "";
+  prix: number = 0;
 
-    showError: boolean = false
+  showError: boolean = false;
 
-    constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) {}
 
-
-    sendProduct() { 
-        if (this.nom.length === 0 || this.prix < 0 || this.prix * 100 % 1 !== 0) {
-            this.showError = true
-            return
-        }
-        this.productService.addProduit({
-            nom: this.nom,
-            prix: this.prix,
-        }).subscribe(() => {
-            window.location.reload()
-        })
+  sendProduct() {
+    if (
+      typeof this.nom !== "string" ||
+      typeof this.prix !== "number" ||
+      this.nom.length === 0 ||
+      this.prix < 0 ||
+      (this.prix * 100) % 1 !== 0
+    ) {
+      this.showError = true;
+      return;
     }
-} 
+    this.productService
+      .addProduit({
+        nom: this.nom,
+        prix: this.prix,
+      })
+      .subscribe(() => {
+        window.location.reload();
+      });
+  }
+}
